91e9f18fda2dde725ea380cd3bd385c6ed22d4c3
GaussYuan191 <1915059302@qq.com>
2021-09-28 13:42:12 +0800

    已更新在线服务器地址

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,6 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_API: '"http://127.0.0.1:8081"'
+  BASE_API: '"http://39.101.202.100:8100"'
+  // BASE_API: '"http://admin-api.macrozheng.com"'
 })
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  BASE_API: '"http://admin-api.macrozheng.com"'
+  BASE_API: '"http://39.101.202.100:8100"'
+  // BASE_API: '"http://admin-api.macrozheng.com"'
 }
